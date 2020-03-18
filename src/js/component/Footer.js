797f29f75e89fc64ecc8da5ce88a1c2ece4e2fcf
Nelson <nlparra.dev@gmail.com>
2020-03-18 01:04:12 +0000

    set up layout

---
+++ src/js/component/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Footer = props => {
+	return (
+		<footer className="page-footer bg-dark">
+			<div className="footer-copyright text-center py-3">
+				© 2020 Copyright:
+				<a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+			</div>
+		</footer>
+	);
+};
